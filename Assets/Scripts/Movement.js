4c99ff9975efe141255fc8a2275275bff7908e24
JonathanGorr <jonathangorr@gmail.com>
2014-04-29 00:12:52 -0400

    transfer

---
+++ Assets/Scripts/Movement.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+ 
+var speed = 10.0;
+ 
+//The vector we are going to move by
+private var moveDirection : Vector3;
+ 
+function FixedUpdate () {
+    //Set moveDirection to the vertical axis (up and down keys) * speed
+    //For smoother movement use Input.GetAxis instead of Input.GetAxisRaw
+    moveDirection = Vector3(0,0,speed*Input.GetAxisRaw("Vertical"));
+    //Transform the vector3 to local space
+    moveDirection = transform.TransformDirection(moveDirection);
+    //set the velocity, so you can move
+    transform.rigidbody.velocity = moveDirection;
+}
